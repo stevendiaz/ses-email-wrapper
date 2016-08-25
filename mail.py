70b42f6d7a047de67214635fdb160c7338ae5a58
Steven Diaz <stevendiaz@utexas.edu>
2016-08-24 20:01:15 -0500

    error checking on html/text setting

--- mail.py
+++ mail.py
@@ -31,6 +31,12 @@ class Message(object):
     def text(self, value):
         self.Message['Body']['Text']['Data'] = value
 
+
+    # Reccommended that this be sent in a concurrently in a background thread 
+    # AWS connection and mail sending can take a significant amount of time
     def send_mail(self, aws_region_name, aws_access_key, aws_secret_access_key):
         ses_client = boto3.client('ses', region_name=aws_region_name, aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_access_key)
+        if self.html is None and self.text is None:
+            raise Exception('You must set an html body or text body')
+        
         ses_client.send_email(**self.__dict__)
